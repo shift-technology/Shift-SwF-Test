2df78c948a87376fda8b297ea5a374bb60197dad
Justin Marshall <justin.marshall@shift-technology.com>
2020-08-22 22:16:53 +0200

    New algorithm

---
+++ Shift-Quality-App/NewAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shift.Quality.App
+{
+    public class NewAlgorithm
+    {
+        public void Compute()
+        {
+            Console.WriteLine("Computing...");
+        }
+    }
+}
